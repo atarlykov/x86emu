c8f27fe18b85106530e04277d08a11404018445e
Aleksei Tarlykov <atarlykov@gmail.com>
2021-01-23 23:03:48 +0300

    validation of clocked configuration

---
+++ src/test/java/at/emu/i8086/ClockedConfigurationTest.java
@@ -0,0 +1,98 @@
+package at.emu.i8086;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Verifies clocked configuration to match the simple original one
+ */
+public class ClockedConfigurationTest {
+
+    /**
+     * compares configurations
+     */
+    @Test
+    public void compare() throws Exception
+    {
+        Cpu cpu = new Cpu();
+
+        // populate original opcodes (assume they are correct)
+        Cpu.Opcode[] sOpcodes = new Cpu.Opcode[256];
+        for (int i = 0; i < cpu.configurations.length; i++) {
+            Cpu.OpcodeConfiguration cfgProvider = cpu.configurations[i];
+            Map<String, ?> configuration = cfgProvider.getConfiguration();
+            Cpu.OpcodeConfiguration.apply(sOpcodes, configuration);
+        }
+
+        // merge clocked configuration
+        Map<String, Cpu.ClockedOpcodeConfiguration.Configuration> clConfig = new HashMap<>();
+        for (int i = 0; i < cpu.configurations.length; i++) {
+            Cpu.ClockedOpcodeConfiguration cfgProvider = (Cpu.ClockedOpcodeConfiguration) cpu.configurations[i];
+            Map<String, Cpu.ClockedOpcodeConfiguration.Configuration> cc = cfgProvider.getClockedConfiguration();
+            Cpu.ClockedOpcodeConfiguration.merge(clConfig, cc);
+        }
+
+        // clocked configuration has much more details,
+        // go through them and compare against the original
+        clConfig.entrySet().stream().sorted(Map.Entry.comparingByKey()).forEach(entry ->
+        {
+            // value could be simple config or mrr
+            String key = entry.getKey();
+            Cpu.ClockedOpcodeConfiguration.Configuration clOpConfig = entry.getValue();
+            // original opcode by that key
+            Cpu.Opcode sOpcode = sOpcodes[Integer.parseInt(key, 2)];
+
+            if (clOpConfig instanceof Cpu.ClockedOpcodeConfiguration.SimpleConfiguration)
+            {
+                // clocked config is simple, original MUST be simple too
+                Cpu.ClockedOpcodeConfiguration.SimpleConfiguration clOpConfigSimple = (Cpu.ClockedOpcodeConfiguration.SimpleConfiguration) clOpConfig;
+                if (sOpcode.getClass() != clOpConfigSimple.opClass) {
+                    throw new RuntimeException("key:" + key +
+                            "  original:" + sOpcode.getClass().getSimpleName() +
+                            "  clocked:" + clOpConfigSimple.opClass);
+                }
+            }
+            else {
+                // clocked is MRR based
+                Cpu.ClockedOpcodeConfiguration.ModRegRmConfiguration clOpConfigMrr = (Cpu.ClockedOpcodeConfiguration.ModRegRmConfiguration) clOpConfig;
+
+                // original could be simple (one class handles all mrrs)
+                if (!(sOpcode instanceof Cpu.RegBasedDemux))
+                {
+                    // all clocked config should match the same class
+                    clOpConfigMrr.mrr.keySet().stream().sorted().forEach( mrr -> {
+                        Cpu.ClockedOpcodeConfiguration.SimpleConfiguration clMrrConfig = clOpConfigMrr.mrr.get(mrr);
+                        if (sOpcode.getClass() != clMrrConfig.opClass) {
+                            throw new RuntimeException("key:" + key +
+                                    "  original:" + sOpcode.getClass().getSimpleName() +
+                                    "  clocked:" + clMrrConfig.opClass);
+                        }
+                    });
+                }
+                // or depend on mrr too
+                else {
+                    Cpu.RegBasedDemux sOpcodeDemux = (Cpu.RegBasedDemux) sOpcode;
+                    clOpConfigMrr.mrr.keySet().stream().sorted().forEach( mrr ->
+                    {
+                        Cpu.ClockedOpcodeConfiguration.SimpleConfiguration clMrrConfig = clOpConfigMrr.mrr.get(mrr);
+                        int reg = (Integer.parseInt(mrr, 2) >> 3) & 0b111;
+                        if (sOpcodeDemux.exits[reg].getClass() != clMrrConfig.opClass) {
+                            throw new RuntimeException("key: " + key + "   reg:" +reg +
+                                    "    opcode: " + sOpcodeDemux.exits[reg].getClass().getSimpleName() +
+                                    "    clocked:" + clMrrConfig.opClass.getSimpleName());
+                        }
+                    });
+                }
+            }
+        });
+
+
+
+        //Assertions.assertEquals(EXPECTED_OUTPUT, result.toString());
+    }
+
+    public static void main(String[] args) throws Exception {
+    }
+}
